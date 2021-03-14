bbc9ca6a155ba986bec940d1b8327a68dc924bc6
pcjerry <jerry.xiongdee@gmail.com>
2021-03-14 21:37:28 +0100

    Start

---
+++ src/App.js
@@ -0,0 +1,71 @@
+/*import React from "react"
+import Signup from "./components/Signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from "./components/Dashboard"
+import Login from "./components/Login"
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./components/ForgotPassword"
+import UpdateProfile from "./components/UpdateProfile"
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App*/
+
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Dashboard from "./components/Login/Dashboard";
+import Signup from "./components/Login/Signup";
+import Login from "./components/Login/Login";
+import ForgotPassword from "./components/Login/ForgotPassword";
+import UpdateProfile from "./components/Login/UpdateProfile";
+import PrivateRoute from "./components/Login/PrivateRoute"
+
+
+import LandingPage from "./components/LandingPage";
+
+
+function App() {
+    return (
+        <>
+            <Router>
+                <AuthProvider>
+                <Switch>
+                    <Route path='/' exact component={LandingPage} />
+                    <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                    <PrivateRoute path="/update-profile" component={UpdateProfile} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/forgot-password" component={ForgotPassword} />
+                </Switch>
+            </AuthProvider>
+            </Router>
+        </>
+    );
+}
+
+export default App;
