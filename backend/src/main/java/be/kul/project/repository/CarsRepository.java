5723df6ae986fc4bb0260702b9b6f417c86f8753
pcjerry <jerry.xiongdee@gmail.com>
2021-03-15 14:24:49 +0100

    Backend deleted

--- backend/src/main/java/be/kul/project/repository/CarsRepository.java
+++
@@ -1,9 +0,0 @@
-package be.kul.project.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import be.kul.project.model.Cars;
-
-public interface CarsRepository extends CrudRepository<Cars, Integer> {
-
-}
--- backend/src/main/java/be/kul/project/repository/HistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package be.kul.project.repository;
-
-import be.kul.project.model.History;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HistoryRepository extends CrudRepository<History, Integer> {
-    Iterable<History> findByEmail(String email);
-}
